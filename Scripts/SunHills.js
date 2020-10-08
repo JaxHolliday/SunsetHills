4cb91c1b4c4a3cff92c175855d06069d0edb9276
Jackson Holliday <jacksonHolliday@users.noreply.github.com>
2020-10-08 12:12:41 -0400

    update

---
+++ Scripts/SunHills.js
@@ -0,0 +1,21 @@
+﻿//Step 1: Add event listener for button click
+let bldgHeight1 = Number(document.getElementById("txtSunsetIn1").value);
+let bldgHeight2 = Number(document.getElementById("txtSunsetIn1").value);
+let bldgHeight3 = Number(document.getElementById("txtSunsetIn1").value);
+let bldgHeight4 = Number(document.getElementById("txtSunsetIn1").value);
+let bldgHeight5 = Number(document.getElementById("txtSunsetIn1").value);
+
+
+//Step 2: Gather user input the put into an array
+var bldgArray = new Array();
+bldgArray.push(bldgHeight1);
+
+
+
+//Step 3: traverse array from right to left. And keep tack of bldgs the can see sunset
+
+
+
+
+
+//Step 4: Out to the user on which buildings can sunset
