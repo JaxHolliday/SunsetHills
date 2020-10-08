6a4e2f4327b9c4826e20060ed287d78f16c8b610
Jackson Holliday <jacksonHolliday@users.noreply.github.com>
2020-10-08 15:49:05 -0400

    bruh

--- Scripts/SunHills.js
+++ Scripts/SunHills.js
@@ -10,7 +10,8 @@
     let bldgHeight4 = Number(document.getElementById("txtSunsetIn4").value);
     let bldgHeight5 = Number(document.getElementById("txtSunsetIn5").value);
 
-    var bldgArray = new Array();
+    var bldgArray = [];
+    var result = [];
     bldgArray.push(bldgHeight1);
     bldgArray.push(bldgHeight2);
     bldgArray.push(bldgHeight3);
@@ -19,13 +20,21 @@
 
 
 
-    //Step 3: traverse array from right to left. And keep tack of bldgs the can see sunset
+    //Step 3: traverse array from right to left. And keep track of bldgs the can see sunset
+    if ()
+    for (let i = bldgArray.length - 1; i >= 2; i--) {
+        if (bldgArray[i] < bldgArray[i - 1]) {
+            result.push(bldgArray[i - 1])
+        } else {
+            result.push(bldgArray[i])
+        }
+    };
 
-
-
+    result.push(bldgArray[0])
+    result.reverse()
 
 
     //Step 4: Out to the user on which buildings can sunset
     //Where result will go
-    document.getElementById("SSoutput").innerHTML = bldgArray;
-}
+    document.getElementById("SSoutput").innerHTML = result;
+};
